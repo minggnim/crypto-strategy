177a1fd597322df8c61b38940f22a2ddbcdfed17
minggnim <38852608+minggnim@users.noreply.github.com>
2022-01-19 23:16:56 +0000

    add tests

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -5,11 +5,16 @@ from crypto_strategy.strategies.bo_strategy import InspectBoStrategy
 
 
 @pytest.fixture
-def positions_prior():
+def xrp_positions_prior():
     return pd.read_pickle('./tests/positions_XRPUSDT-4h-bo-100-15-ts_stop-0.10-vol-10-2-20211230.pkl')
 
 
-def test_regression(positions_prior):
+@pytest.fixture
+def btc_positions_prior():
+    return pd.read_pickle('./tests/positions_BTCUSDT-4h-bo-165-50-ts_stop-0.15-vol-30-1-20211207.pkl')
+
+
+def test_regression_xrp(xrp_positions_prior):
     stats = InspectBoStrategy(
         'XRPUSDT', '4h', 
         long_window=100, short_window=15, 
@@ -18,5 +23,18 @@ def test_regression(positions_prior):
 
     positions = stats.portfolio.positions.records_readable
     positions = positions[positions["Entry Timestamp"] < "2021-12-30"]
-    np.testing.assert_allclose(positions["Size"].values, positions_prior["Size"].values)
-    np.testing.assert_equal(positions['Entry Timestamp'].values, positions_prior['Entry Date'].values)
+    np.testing.assert_allclose(positions["Size"].values, xrp_positions_prior["Size"].values)
+    np.testing.assert_equal(positions['Entry Timestamp'].values, xrp_positions_prior['Entry Date'].values)
+
+
+def test_regression_btc(btc_positions_prior):
+    stats = InspectBoStrategy(
+        'BTCUSDT', '4h', 
+        long_window=165, short_window=50, 
+        flag_ts_stop=True, ts_stop=0.15, 
+        flag_filter='vol', timeperiod=30, multiplier=1, show_fig=False)
+    
+    positions = stats.portfolio.positions.records_readable
+    positions = positions[positions["Entry Timestamp"] < "2021-12-30"]
+    np.testing.assert_allclose(positions["Size"].values, btc_positions_prior["Size"].values)
+    np.testing.assert_equal(positions['Entry Timestamp'].values, btc_positions_prior['Entry Date'].values)
